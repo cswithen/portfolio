527386003e1edf6163af2574f38fb18f8720a3fa
cswithen <cody.swithenbank@gmail.com>
2022-01-26 07:53:42 -0800

    style: skill card hexagons
    skill cards use font awesome icons as well as have an animating hexagon behind them. Animation used multiple animations to start from different background positions and are based on properties on the data object

--- components/AboutSkillsCard.js
+++ components/AboutSkillsCard.js
@@ -1,11 +1,25 @@
 import React from "react";
 
+//components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+//font awesome icons
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 //styles
 import styles from "../styles/AboutSkillCard.module.css";
 
-const AboutSkillCard = ({ skill }) => {
+const AboutSkillCard = ({ skill, index }) => {
   return (
     <div className={styles.skillCard}>
+      <div
+        className={`${styles.hexagon} ${styles[skill.animation]}`}
+        style={{ backgroundPosition: `${index * 50}%` }}
+      >
+        <div className={styles.faIcon}>
+          <FontAwesomeIcon icon={faUser} />
+        </div>
+      </div>
       <h3>{skill.name}</h3>
       <div>{skill.tagline}</div>
     </div>
