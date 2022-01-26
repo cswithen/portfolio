11d4ba5ff70dbec008f85e0674e7d258a8a1e52a
cswithen <cody.swithenbank@gmail.com>
2022-01-26 11:13:34 -0800

    feat: call to action
    added button in hero section as a call to action to check out project work
    
    closes #63

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+//components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+//font awesome icons
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 // styles
 import styles from "../styles/Hero.module.css";
 
@@ -10,7 +16,12 @@ const Hero = () => {
       <div className={styles.heroText}>
         Hello, my name is{" "}
         <span className={styles.nameHighlight}>Cody Swithenbank</span>. <br />
-        And I am a full-stack web developer.
+        And I am a full-stack web developer. <br />
+        <br />
+        <a href="#projects" className={styles.button}>
+          Check my work{" "}
+          <FontAwesomeIcon icon={faArrowRight} className={styles.arrow} />
+        </a>
       </div>
       <div className={styles.canvas}></div>
     </div>
