8aac6ac9ab48312d99076b8d72fbacfe4f0c7b56
cswithen <cody.swithenbank@gmail.com>
2022-01-26 07:44:51 -0800

    build: font awesome config
    added in configuration in the _app file overwrite fontAwesome autoAddCss property to prevent erratic and skewed loading into the pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+// font awesome config to prevent autoAdding css
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+// global styles
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
